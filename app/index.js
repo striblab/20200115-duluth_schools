3f111c201d83f3ae51719c21c5d530df0ad80ab2
Frey Hargarten <harga012@umn.edu>
2020-01-16 11:39:38 -0600

    updates

--- app/index.js
+++ app/index.js
@@ -405,4 +405,4 @@ $(".reset").on("click", function(){
 });
 
 
-!function(){"use strict";window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}})}();
+!function(){"use strict";window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector(".dataframe iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}})}();
